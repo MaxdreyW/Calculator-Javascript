be2250400e12ec663dd89278fff3ae4d15ddcc94
Marx520 <maxdrey_wong@yahoo.com>
2023-07-19 12:00:46 +0700

    add dark-mode

---
+++ dark-mode.js
@@ -0,0 +1,11 @@
+let Main = document.querySelector(".main");
+let Result = document.querySelector(".result");
+let Button = document.querySelectorAll(".button");
+let DarkMode = document.querySelector("#dark-mode-icon");
+let body = document.body;
+DarkMode.addEventListener("click", () => {
+  Main.classList.toggle("main-dark");
+  Result.classList.toggle("result-dark");
+  body.classList.toggle("dark-mode");
+  Button.forEach((b) => b.classList.toggle("Button-dark"));
+});
